2e8ed11ecd13c11ac117dd8c05b0d2bcee178ce4
kkawakami08 <k.kawakami08@gmail.com>
2022-02-07 17:12:51 -0800

    finalize

--- javascript.js
+++ javascript.js
@@ -13,53 +13,72 @@ let playerSelection = playerPrompt.toLowerCase();
 
 console.log(`This is player: ${playerSelection}`);
 
-function playRound(playerSelection, computerSelection) { 
-    if (computerSelection === "rock") {
-        switch (playerSelection) {
-            case "rock":
-                alert("It's a tie!");
-                break;
-            case "paper":
-                alert("You win! Paper beats rock");
-                break;
-            case "scissors":
-                alert("You lose. Rock beats scissors");
-                break;
-            default:
-                alert("");
-                
-        }
-    } else if (computerSelection === "paper") {
-        switch (playerSelection) {
-            case "rock":
-                alert("You lose! Paper beats rock.");
-                break;
-            case "paper":
-                alert("It's a tie!");
-                break;
-            case "scissors":
-                alert("You win! Scissors beats paper.");
-                break;
-            default:
-                alert("");
-                
-        }
-    } else {
-        switch (playerSelection) {
-            case "rock":
-                alert("You win! Rock beats scissors");
-                break;
-            case "paper":
-                alert("You lose, scissors beats paper");
-                break;
-            case "scissors":
-                alert("It's a tie!");
-                break;
-            default:
-                alert("");
-                
+let playerScore = 0;
+let computerScore = 0;
+
+function game() {
+ 
+
+    function playRound(playerSelection, computerSelection) { 
+        if (computerSelection === "rock") {
+            switch (playerSelection) {
+                case "rock":
+                    alert("It's a tie!");
+                    break;
+                case "paper":
+                    alert("You win! Paper beats rock");
+                    playerScore++;
+                    break;
+                case "scissors":
+                    alert("You lose. Rock beats scissors");
+                    computerScore++;
+                    break;
+                default:
+                    alert("");
+                    
+            }
+        } else if (computerSelection === "paper") {
+            switch (playerSelection) {
+                case "rock":
+                    alert("You lose! Paper beats rock.");
+                    computerScore++;
+                    break;
+                case "paper":
+                    alert("It's a tie!");
+                    break;
+                case "scissors":
+                    alert("You win! Scissors beats paper.");
+                    playerScore++;
+                    break;
+                default:
+                    alert("");
+                    
+            }
+        } else {
+            switch (playerSelection) {
+                case "rock":
+                    alert("You win! Rock beats scissors");
+                    playerScore++;
+                    break;
+                case "paper":
+                    alert("You lose, scissors beats paper");
+                    computerScore++;
+                    break;
+                case "scissors":
+                    alert("It's a tie!");
+                    break;
+                default:
+                    alert("");
+                    
+            }
         }
     }
+    return playRound(playerSelection,computerSelection);
+
+    
 }
    
-console.log(playRound(playerSelection,computerSelection));
+console.log(game());
+
+console.log(playerScore);
+console.log(computerScore);
