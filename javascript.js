43b38c94d044eb6b8db8fd37707cb31b1387d6ce
kkawakami08 <k.kawakami08@gmail.com>
2022-02-07 16:59:33 -0800

    add playRound function

--- javascript.js
+++ javascript.js
@@ -6,6 +6,60 @@ function computerPlay() {
 
 let computerSelection = computerPlay();
 
-console.log(computerSelection);
+console.log(`This is computer: ${computerSelection}`);
 
+let playerPrompt = prompt("Rock, Paper, or Scissors?");
+let playerSelection = playerPrompt.toLowerCase();
 
+console.log(`This is player: ${playerSelection}`);
+
+function playRound(playerSelection, computerSelection) { 
+    if (computerSelection === "rock") {
+        switch (playerSelection) {
+            case "rock":
+                alert("It's a tie!");
+                break;
+            case "paper":
+                alert("You win! Paper beats rock");
+                break;
+            case "scissors":
+                alert("You lose. Rock beats scissors");
+                break;
+            default:
+                alert("");
+                
+        }
+    } else if (computerSelection === "paper") {
+        switch (playerSelection) {
+            case "rock":
+                alert("You lose! Paper beats rock.");
+                break;
+            case "paper":
+                alert("It's a tie!");
+                break;
+            case "scissors":
+                alert("You win! Scissors beats paper.");
+                break;
+            default:
+                alert("");
+                
+        }
+    } else {
+        switch (playerSelection) {
+            case "rock":
+                alert("You win! Rock beats scissors");
+                break;
+            case "paper":
+                alert("You lose, scissors beats paper");
+                break;
+            case "scissors":
+                alert("It's a tie!");
+                break;
+            default:
+                alert("");
+                
+        }
+    }
+}
+   
+console.log(playRound(playerSelection,computerSelection));
